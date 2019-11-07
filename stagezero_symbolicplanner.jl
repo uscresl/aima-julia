c923adfa5f805b4750f88d796c161a137269fc03
Gautam Salhotra <gautams3@users.noreply.github.com>
2019-11-06 18:21:24 -0800

    Added pdb-style debugging

--- stagezero_symbolicplanner.jl
+++ stagezero_symbolicplanner.jl
@@ -1,85 +1,67 @@
 # Our own symbolic graph
 
+using Debugger
+
 include("aimajulia.jl");
 using Main.aimajulia;
 
-knowledge_base = [
-    expr("Connected(Bucharest,Pitesti)"),
-    expr("Connected(Pitesti,Rimnicu)"),
-    expr("Connected(Rimnicu,Sibiu)"),
-    expr("Connected(Sibiu,Fagaras)"),
-    expr("Connected(Fagaras,Bucharest)"),
-    expr("Connected(Pitesti,Craiova)"),
-    expr("Connected(Craiova,Rimnicu)"),
-];
-
-for element in [
+function define()
+    @bp
+    knowledge_base = [
+        expr("At(START)"), 
         expr("Empty(S1) & Empty(S2) & !Empty(C1) &!Empty(C2) & Empty(arm) & Closed(D1) & Closed(D2) ==> At(START)"),
         expr("Empty(S1) & Empty(S2) & !Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS1)"),
-        expr("Empty(S1) & Empty(S2) & Empty(C1) &!Empty(C2) & !Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS2)"),
-        expr("!Empty(S1) & Empty(S2) & Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS3)"),
-        expr("Empty(S1) & !Empty(S2) & Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS4)"),
-        expr("At(START)")
-    ]
-    push!(knowledge_base, element);
+        ];
+
+    # DOESN'T WORK
+    # for element in [
+    #         expr("Empty(S1) & Empty(S2) & !Empty(C1) &!Empty(C2) & Empty(arm) & Closed(D1) & Closed(D2) ==> At(START)"),
+    #         expr("Empty(S1) & Empty(S2) & !Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS1)"),
+    #         # expr("Empty(S1) & Empty(S2) & Empty(C1) &!Empty(C2) & !Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS2)"),
+    #         # expr("!Empty(S1) & Empty(S2) & Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS3)"),
+    #         # expr("Empty(S1) & !Empty(S2) & Empty(C1) &!Empty(C2) & Empty(arm) & !Closed(D1) & Closed(D2) ==> At(SS4)"),
+            
+    #         # expr("Empty(S1) & Empty(S2) & !Empty(C1) &!Empty(C2) & Empty(arm) & Closed(D1) & !Closed(D2) ==> At(SS5)"),
+    #         # expr("Empty(S1) & Empty(S2) & !Empty(C1) &Empty(C2) & !Empty(arm) & Closed(D1) & !Closed(D2) ==> At(SS6)"),
+    #         # expr("!Empty(S1) & Empty(S2) & !Empty(C1) &Empty(C2) & Empty(arm) & Closed(D1) & !Closed(D2) ==> At(SS7)"),
+    #         # expr("Empty(S1) & !Empty(S2) & !Empty(C1) &Empty(C2) & Empty(arm) & Closed(D1) & !Closed(D2) ==> At(SS8)"),
+            
+    #         # expr("At(START)")
+    #     ]
+    #     push!(knowledge_base, element);
+    # end
+
+    # Open D1
+    precond_pos = [expr("Closed(D1)")];
+    precond_neg = [];
+    effect_add = [expr("!Closed(D1)")];
+    effect_rem = [expr("Closed(D1)")];
+    open_d1 = PlanningAction(expr("OpenD1"), (precond_pos, precond_neg), (effect_add, effect_rem));
+
+    # Open D2
+    precond_pos = [expr("Closed(D2)"), expr("Empty(arm)")];
+    precond_neg = [];
+    effect_add = [expr("!Closed(D2)")];
+    effect_rem = [expr("Closed(D2)")]; # arm is still empty after the action
+    open_d2 = PlanningAction(expr("OpenD2"), (precond_pos, precond_neg), (effect_add, effect_rem));
+
+    # pick C1
+    precond_pos = [expr("!Empty(C1)"), expr("Empty(arm)")];
+    precond_neg = [];
+    effect_add = [expr("Empty(C1)"), expr("!Empty(arm)")];
+    effect_rem = [expr("!Empty(C1)"), expr("Empty(arm)")];
+    pick_c1 = PlanningAction(expr("PickC1"), (precond_pos, precond_neg), (effect_add, effect_rem));
+
+    function goal_test(kb::PDDL)
+        return ask(kb, expr("At(SS1)"));
+    end
+
+    prob = PDDL(knowledge_base, [open_d1, open_d2, pick_c1], goal_test)
+
+    fol_kb = FirstOrderLogicKnowledgeBase(knowledge_base)
+    gpp = GraphPlanProblem(prob, fol_kb)
+
+    graphplan(gpp, ([expr("At(START)")], []))
 end
 
-# Sibiu to Bucharest
-precond_pos = [expr("At(Sibiu)")];
-precond_neg = [];
-effect_add = [expr("At(Bucharest)")];
-effect_rem = [expr("At(Sibiu)")];
-fly_s_b = PlanningAction(expr("Fly(Sibiu, Bucharest)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Bucharest to Sibiu
-precond_pos = [expr("At(Bucharest)")];
-precond_neg = [];
-effect_add = [expr("At(Sibiu)")];
-effect_rem = [expr("At(Bucharest)")];
-fly_b_s = PlanningAction(expr("Fly(Bucharest, Sibiu)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Sibiu to Craiova
-precond_pos = [expr("At(Sibiu)")];
-precond_neg = [];
-effect_add = [expr("At(Craiova)")];
-effect_rem = [expr("At(Sibiu)")];
-fly_s_c = PlanningAction(expr("Fly(Sibiu, Craiova)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Craiova to Sibiu
-precond_pos = [expr("At(Craiova)")];
-precond_neg = [];
-effect_add = [expr("At(Sibiu)")];
-effect_rem = [expr("At(Craiova)")];
-fly_c_s = PlanningAction(expr("Fly(Craiova, Sibiu)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Bucharest to Craiova
-precond_pos = [expr("At(Bucharest)")];
-precond_neg = [];
-effect_add = [expr("At(Craiova)")];
-effect_rem = [expr("At(Bucharest)")];
-fly_b_c = PlanningAction(expr("Fly(Bucharest, Craiova)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Craiova to Bucharest
-precond_pos = [expr("At(Craiova)")];
-precond_neg = [];
-effect_add = [expr("At(Bucharest)")];
-effect_rem = [expr("At(Craiova)")];
-fly_c_b = PlanningAction(expr("Fly(Craiova, Bucharest)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-# Drive
-precond_pos = [expr("At(x)"), expr("Connected(x,y)")];
-precond_neg = [];
-effect_add = [expr("At(y)")];
-effect_rem = [expr("At(x)")];
-drive = PlanningAction(expr("Drive(x, y)"), (precond_pos, precond_neg), (effect_add, effect_rem));
-
-function goal_text(kb::PDDL)
-    return ask(kb, expr("At(Bucharest)"));
-end
-
-prob = PDDL(knowledge_base, [fly_s_b, fly_b_s, fly_s_c, fly_c_s, fly_b_c, fly_c_b, drive], goal_test)
-
-folkb = FirstOrderLogicKnowledgeBase(knowledge_base)
-gpp = GraphPlanProblem(prob, folkb)
-
-graphplan(gpp, ([expr("At(Bucharest)")], []))
+@enter define()
